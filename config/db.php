903df97ca2f225387dbb8973446d88a3aff7155a
Flewid <m.gibson@flewid.ca>
2018-10-24 00:33:39 -0400

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=staks_demo',
-    'username' => 'staks_demo',
-    'password' => 'yqnDVZu4sDguNIcg',
-    'charset' => 'utf8',
-];
