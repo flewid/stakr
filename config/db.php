99ca2da6f8a0d7f97d1e6ec1a14630ce222169f3
Flewid <m.gibson@flewid.ca>
2018-10-24 00:11:09 -0400

    First Push To GitHub

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=staks_demo',
+    'username' => 'staks_demo',
+    'password' => 'yqnDVZu4sDguNIcg',
+    'charset' => 'utf8',
+];
